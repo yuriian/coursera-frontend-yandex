1a56544e35ee9453a8b8f188d2e3a195acad7557
Yurii Antipov <yuriy.antipov85@gmail.com>
2018-06-08 13:24:55 +0300

    Memoji: created branch for only Memoji project

--- c5_final-project/classmates-works/week1/Коктыш_Тихон/script.js
+++
@@ -1,9 +0,0 @@
-﻿var cards = document.querySelectorAll('.card');
-
- for (var i = 0 ; i < cards.length ; i++) {
-     cards[i].addEventListener('click', function () {
-        this.classList.contains('chosen') ?
-            this.classList.remove('chosen') :
-            this.classList.add('chosen');
-     });
- }
